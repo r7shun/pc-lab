aec2c4c3c608c03015273fc462d7a9cf6bb2e9c5
Licht56 <wushunyu2401@mail.ustc.edu.cn>
2020-07-06 14:21:38 +0800

    unicode

--- src/lab4/lab4.c
+++ src/lab4/lab4.c
@@ -4,7 +4,7 @@
 #include "mpi.h"
 #define INF 2147483647
 
-// ????????????A[l : m], A[m + 1 : r], ??A[l : r]
+// 合并两个已排好序的子数组A[l : m], A[m + 1 : r], 写回A[l : r]
 void Merge(int *A, int l, int m, int r)
 {
     int i, j, k, n1 = m - l + 1, n2 = r - m;
@@ -25,7 +25,7 @@ void Merge(int *A, int l, int m, int r)
     free(R);
 }
 
-// ?A[l : r]??????
+// 对A[l : r]进行归并排序
 void MergeSort(int *A, int l, int r)
 {
     if (l < r)
@@ -37,7 +37,7 @@ void MergeSort(int *A, int l, int r)
     }
 }
 
-// ?A[0 : n - 1]??PSRS??
+// 对A[0 : n - 1]进行PSRS排序
 void PSRS(int *A, int n, int id, int num_processes)
 {
     int per;
@@ -54,25 +54,25 @@ void PSRS(int *A, int n, int id, int num_processes)
     {
         global_samples = (int *)malloc(num_processes * num_processes * sizeof(int));
     }
-    // ???????????
+    // 设置路障，同步所有进程
     MPI_Barrier(MPI_COMM_WORLD);
-    // ????
+    // 均匀划分
     MergeSort(A, id * per, (id + 1) * per - 1);
-    // ??????????? num_processes ?????local_sample?
+    // 正则采样，当前进程选出 num_processes 个样本放在local_sample中
     for (int k = 0; k < num_processes; k++)
         samples[k] = A[id * per + k * per / num_processes];
-    // ????sample??????local_sample?? num_processes * num_processes ?
+    // 主进程的sample收集各进程的local_sample，共 num_processes * num_processes 个
     MPI_Gather(samples, num_processes, MPI_INT, global_samples, num_processes, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // ???? ????
+    // 采样排序 选择主元
     if (id == 0)
     {
         MergeSort(global_samples, 0, num_processes * num_processes - 1);
         for (int k = 0; k < num_processes - 1; k++)
             pivots[k] = global_samples[(k + 1) * num_processes];
-        pivots[num_processes - 1] = INF; //??
+        pivots[num_processes - 1] = INF; //哨兵
     }
-    //????????????????????pivots??
+    //主进程向各个进程广播，所有进程拥有一样的pivots数组
     MPI_Bcast(pivots, num_processes, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
 
@@ -80,7 +80,7 @@ void PSRS(int *A, int n, int id, int num_processes)
     offsets = (int *)calloc(num_processes, sizeof(int));
     newsizes = (int *)calloc(num_processes, sizeof(int));
     newoffsets = (int *)calloc(num_processes, sizeof(int));
-    // ????
+    // 主元划分
     for (int k = 0, j = id * per; j < id * per + per; j++)
     {
         if (A[j] < pivots[k])
@@ -88,10 +88,10 @@ void PSRS(int *A, int n, int id, int num_processes)
         else
             sizes[++k]++;
     }
-    // ????
-    // ??????????????????????????????[????]
+    // 全局交换
+    // 多对多全局交换消息，首先每个进程向每个接收者发送接收者对应的[段的大小]
     MPI_Alltoall(sizes, 1, MPI_INT, newsizes, 1, MPI_INT, MPI_COMM_WORLD);
-    // ?????????????????????????
+    // 计算原来的段偏移数组，新的段偏移数组，新的数据大小
     newdatassize = newsizes[0];
     for (int k = 1; k < num_processes; k++)
     {
@@ -99,30 +99,30 @@ void PSRS(int *A, int n, int id, int num_processes)
         newoffsets[k] = newoffsets[k - 1] + newsizes[k - 1];
         newdatassize += newsizes[k];
     }
-    // ????????????
+    // 申请当前进程新的数据空间
     newdatas = (int *)malloc(newdatassize * sizeof(int));
-    // ???????????????????????????????
+    // 多对多全局交换消息，每个进程向每个接收者发送接收者对应的【段】
     MPI_Alltoallv(&(A[id * per]), sizes, offsets, MPI_INT, newdatas, newsizes, newoffsets, MPI_INT, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
-    // ??????????????
+    // 当前进程归并排序自己的新数据
     MergeSort(newdatas, 0, newdatassize - 1);
     MPI_Barrier(MPI_COMM_WORLD);
-    // ???????????????A
-    // ?????????????
+    // 主进程收集各个进程的数据，写回A
+    // 首先收集各进程新数据的大小
     if (id == 0)
         global_sizes = (int *)calloc(num_processes, sizeof(int));
     MPI_Gather(&newdatassize, 1, MPI_INT, global_sizes, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    // ????????????????????
+    // 主进程计算即将搜集的各进程数据的起始位置
     if (id == 0)
     {
         global_offsets = (int *)calloc(num_processes, sizeof(int));
         for (int k = 1; k < num_processes; k++)
             global_offsets[k] = global_offsets[k - 1] + global_sizes[k - 1];
     }
-    // ????????????
+    // 主进程收集各个进程的数据
     MPI_Gatherv(newdatas, newdatassize, MPI_INT, A, global_sizes, global_offsets, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
-    // ??????
+    // 销毁动态数组
     free(samples);
     samples = NULL;
     free(pivots);
@@ -164,8 +164,8 @@ int main(int argc, char *argv[])
     double t1, t2;
     int id, num_processes;
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &num_processes); //?????
-    MPI_Comm_rank(MPI_COMM_WORLD, &id);            //??????id
+    MPI_Comm_size(MPI_COMM_WORLD, &num_processes); //获取进程数
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);            //获取当前进程id
     if (id == 0)
         t1 = MPI_Wtime();
     PSRS(A, N, id, num_processes);
